9a6d8239a735f26e2b24e0d8d3da7809dc3c5215
Fenky Tjhang <cornelis@hotmail.com>
2025-09-25 12:57:14 +0700

    Setting up log

--- Program.cs
+++ Program.cs
@@ -1,36 +1,77 @@
 
+using Newtonsoft.Json.Linq;
+using Serilog;
+
 namespace RVMSService
 {
     public class Program
     {
+
+        public static JObject SettingsConfig { get; set; }
+        public static string FileSettings { get; set; }
+        public string ConnString { get; set; }
+        public string serverAddress { get; set; }
+
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            //Setting up to read json settings file and ensure that it will read on the application directory
+            string exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string sharedSettingsPath = Path.Combine(exeDirectory, @"Settings\Settings.json");
+            sharedSettingsPath = Path.GetFullPath(sharedSettingsPath);
 
-            // Add services to the container.
+            // Configure Serilog to log to a file
+            string logPath = Path.Combine(exeDirectory, "Logs", "log-.txt");
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                SettingsConfig = JObject.Parse(System.IO.File.ReadAllText(sharedSettingsPath));
+                string url = $"http://{SettingsConfig["ServerAddress"]?.ToString() ?? "localhost"}";
+
+                Log.Information("Starting web host");
+
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Host.UseWindowsService();
+                builder.Host.UseSerilog();
+                builder.WebHost.UseUrls(url);
+
+
+                // Add services to the container.
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+                app.UseAuthentication();
+                app.UseAuthorization();
+
+
+                app.MapControllers();
+
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
